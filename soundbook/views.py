b366bdc53995a4edbc19b13c07cd087bbbc78f4c
akashgupta77500 <akashgupta77500@gmail.com>
2021-09-19 16:14:46 +0530

    soundbook

---
+++ soundbook/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render,redirect
+import pyttsx3
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def resumeread(request):
+    if request.method == "POST" :
+        value = request.POST["honey"]
+        engine = pyttsx3.init()
+        engine.say(value)
+        engine.runAndWait()
+        return redirect('/')
+
+
+
